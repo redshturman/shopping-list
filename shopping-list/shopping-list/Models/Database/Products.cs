2ef6700d63bac2c01bcb6efe5ec23235b54d7c72
Red_PC <jovefernandez@gmail.com>
2025-03-25 15:26:47 +0100

    red-11 Created first model and enum for the app

---
+++ shopping-list/shopping-list/Models/Database/Products.cs
@@ -0,0 +1,31 @@
+﻿
+using shopping_list.Models.Enums;
+
+namespace shopping_list.Models.Database
+{
+    /// <summary>
+    /// The products database model.
+    /// </summary>
+    public class Products
+    {
+        /// <summary>
+        /// The product id.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The product name.
+        /// </summary>
+        public string Name { get; set; } = string.Empty;
+
+        /// <summary>
+        /// The product description.
+        /// </summary>
+        public string Description { get; set; } = string.Empty;
+
+        /// <summary>
+        /// The supermarket where the product is located.
+        /// </summary>
+        public Supermarkets Supermarket { get; set; }
+    }
+}
