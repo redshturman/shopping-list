9a9b9aac7b01fa293f360582231e9cbe374ce48b
Red_PC <jovefernandez@gmail.com>
2025-03-26 21:18:55 +0100

    red-11 Added functionality in the backend

---
+++ shopping-list/shopping-list/APIs/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using shopping_list.Services.Database;
+
+namespace shopping_list.APIs
+{
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ProductsDatabaseService _productsDatabaseService;
+
+        public ProductsController(ProductsDatabaseService productsDatabaseService)
+        {
+            _productsDatabaseService = productsDatabaseService;
+        }
+
+        [HttpGet("GetProducts")]
+        public async Task<IActionResult> GetProductsAsync()
+        {
+            var products = await _productsDatabaseService.GetProductsAsync();
+            return Ok(products);
+        }
+
+        [HttpPost("GenerateSimpleProduct")]
+        public async Task<IActionResult> GenerateSimpleProduct(string productName)
+        {
+            await _productsDatabaseService.GenerateSimpleProduct(productName);
+            return Ok();
+        }
+    }
+}
